ac1f36336acb956b7a4d8648f1a7d1d958a07bf2
Patai Gergely <patai.gergely@gmail.com>
2009-06-13 20:29:33 +0000

    Core update: added stopper action and profiling parameter structures, and switched to bytestrings.

---
+++ core/Profiling/Heap/Process.hs
@@ -0,0 +1,110 @@
+{-| This is a utility module to aid the construction of
+'CreateProcess' structures with profiling parameters. -}
+
+module Profiling.Heap.Process 
+    ( ProfParam(..)
+    , Breakdown(..)
+    , Restriction(..)
+    , processToProfile
+    ) where
+
+import System.Process
+import Text.Printf
+
+{-| The possible types of parameters. -}
+data ProfParam
+    -- | The type of breakdown.
+    = PPBreakdown Breakdown
+    -- | An additional filter on the runtime side.
+    | PPRestriction Restriction [String]
+    -- | Sampling interval in seconds.
+    | PPInterval Float
+    -- | Whether to include memory taken up by threads.
+    | PPIncludeThreads
+    -- | The maximum length of cost centre stack names.
+    | PPNameLength Int
+    -- | Retainer set size limit.
+    | PPRetainerLimit Int
+
+{-| The possible types of breakdowns. -}
+data Breakdown
+    -- | Breakdown by cost centre stack (origin of the data).
+    = BCostCentreStack
+    -- | Breakdown by module (code responsible for the data).
+    | BModule
+    -- | Breakdown by closure description (constructor name or some
+    -- unique identifier).
+    | BDescription
+    -- | Breakdown by type (or an approximation if it is not known
+    -- exactly).
+    | BType
+    -- | Breakdown by retainer set (effectively the entities that hold
+    -- a direct reference to the data in question).
+    | BRetainer
+    -- | Breakdown by biography (phase of an object's lifetime).
+    | BBiography
+
+{-| The possible filters. Note that these are imposed by the runtime,
+so we cannot override them on the application side. -}
+data Restriction
+    -- | Show only closures with one of the given names on the top of
+    -- the cost centre stack.
+    = RCCStackTop
+    -- | Show only closures with one of the given names somewhere in
+    -- the cost centre stack.
+    | RCCStackAny
+    -- | Show only closures produced by one of the given modules.
+    | RModule
+    -- | Show only closures with a description that matches one of the
+    -- given names.
+    | RDescription
+    -- | Show only closures with one of the given types.
+    | RType
+    -- | Show only closures with retainer sets that contain at least
+    -- one cost centre stack with a given name on the top.
+    | RRetainer
+    -- | Show only closures with one of the specified biographies,
+    -- which must come from the set {lag, drag, void, use}.
+    | RBiography
+
+instance Show ProfParam where
+    showsPrec _ (PPBreakdown b)      = showString "-h" . shows b
+    showsPrec _ (PPRestriction r ns) = showString "-h" . shows r . showsNames ns
+    showsPrec _ (PPInterval i)       = showString "-i" . showString (printf "%.2f" i)
+    showsPrec _ (PPIncludeThreads)   = showString "-xt"
+    showsPrec _ (PPNameLength l)     = showString "-L" . shows l
+    showsPrec _ (PPRetainerLimit l)  = showString "-R" . shows l
+    showList ps = foldr (\p s -> shows p . showChar ' ' . s) id ps
+
+instance Show Breakdown where
+    showsPrec _ BCostCentreStack = showChar 'c'
+    showsPrec _ BModule          = showChar 'm'
+    showsPrec _ BDescription     = showChar 'd'
+    showsPrec _ BType            = showChar 'y'
+    showsPrec _ BRetainer        = showChar 'r'
+    showsPrec _ BBiography       = showChar 'b'
+
+instance Show Restriction where
+    showsPrec _ RCCStackTop  = showChar 'c'
+    showsPrec _ RCCStackAny  = showChar 'C'
+    showsPrec _ RModule      = showChar 'm'
+    showsPrec _ RDescription = showChar 'd'
+    showsPrec _ RType        = showChar 'y'
+    showsPrec _ RRetainer    = showChar 'r'
+    showsPrec _ RBiography   = showChar 'b'
+
+showsNames :: [String] -> String -> String
+showsNames []     = id
+showsNames [n]    = showString n
+showsNames (n:ns) = showString n . showChar ',' . showsNames ns
+
+{-| A helper function to create a 'CreateProcess' structure. -}
+processToProfile :: FilePath       -- ^ The executable to profile (relative paths start from the working directory).
+                 -> Maybe FilePath -- ^ An optional working directory (inherited from the parent if not given).
+                 -> String         -- ^ The list of parameters to pass to the program.
+                 -> [ProfParam]    -- ^ Profiling parameters.
+                 -> CreateProcess  -- ^ The resulting structure.
+processToProfile exec dir params profParams = (shell cmd) { cwd = dir }
+    where -- Note: this doesn't handle --RTS in the param string!
+          cmd = exec ++ ' ' : params ++
+                (if null profParams then "" else " +RTS " ++ show profParams)
--- core/Profiling/Heap/Read.hs
+++ core/Profiling/Heap/Read.hs
@@ -2,25 +2,36 @@
 access for others. It is hardly more than some glue code that hides
 the actual profile format from the application. It also provides basic
 data management for applications that don't want to deal with it
-themselves -}
+themselves. -}
 
 module Profiling.Heap.Read where
 
-import Control.Applicative
-import Control.Monad
-import Control.Concurrent
+-- The imperative bits
+import Control.Applicative ((<$>))
+import Control.Monad (when, replicateM_)
+import Control.Concurrent (forkIO, threadDelay)
 import Data.IORef
-import Data.List
-import Data.Maybe
-import qualified Data.IntMap as IM
-import qualified Data.Map as M
-import Data.Time.LocalTime
-import Data.Time.Format
 import System.Directory
 import System.FilePath
 import System.IO
-import System.Locale
 import System.Process
+
+-- Data structures
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as S
+import Data.List
+import Data.Maybe
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Trie (Trie)
+import qualified Data.Trie as T
+
+-- Stuff needed only to create a time stamp
+import Data.Time.LocalTime (getZonedTime)
+import Data.Time.Format (formatTime)
+import System.Locale (defaultTimeLocale)
 
 -- * Data types
 
@@ -36,32 +47,30 @@ an auxiliary map from times to indices would obviously defeat the
 purpose of using it. In the end, a dynamically growing array might be
 the ultimate solution... -}
 
-data Profile = Profile {
-      profData :: M.Map Time ProfileSample,
-      profNames :: IM.IntMap CostCentreName,
-      -- This one should be a trie, e.g. from bytestring-trie,
-      -- otherwise it takes ages to find the id... For the time being
-      -- it is a simple map to avoid extra dependencies.
-      profNamesInv :: ProfNamesInv,
-      profJob :: String,
-      profDate :: String
+data Profile = Profile
+    { profData :: Map Time ProfileSample
+    , profNames :: IntMap CostCentreName
+    , profNamesInv :: Trie CostCentreId
+    , profJob :: String
+    , profDate :: String
     }
 
-type ProfNamesInv = M.Map CostCentreName CostCentreId
-
 instance Show Profile where
-    show p = unlines $ ["Job: " ++ profJob p,"Date: " ++ profDate p,"Name mappings:"] ++
+    show p = unlines $
+             ["Job: " ++ profJob p
+             ,"Date: " ++ profDate p
+             ,"Name mappings:"] ++
              (map show . IM.assocs . profNames) p ++
              ["Measurements (" ++ (show . M.size . profData) p ++ "):"] ++
              (map show . M.assocs . profData) p
 
 emptyProfile :: Profile
-emptyProfile = Profile {
-                 profData = M.empty,
-                 profNames = IM.empty,
-                 profNamesInv = M.empty,
-                 profJob = "",
-                 profDate = ""
+emptyProfile = Profile
+               { profData = M.empty
+               , profNames = IM.empty
+               , profNamesInv = T.empty
+               , profJob = ""
+               , profDate = ""
                }
 
 {-| Cost centres are identified by integers for simplicity (so we can
@@ -73,7 +82,7 @@ type CostCentreId = Int
 splitting them at this point. It can be done later if the application
 needs it. -}
 
-type CostCentreName = String
+type CostCentreName = ByteString
 
 {-| It's a good question how time should be represented. If we use
 maps, we might want to use an IntMap for the profiling data
@@ -87,7 +96,7 @@ type Time = Double
 
 {-| A sampling point is simply a list of cost centres with the
 associated cost. I don't see any need for a fancy data structure here,
-since we normally process every value in this collection, and it' s
+since we normally process every value in this collection, and it's
 usually not big either, only holding a few dozen entries at most. -}
 
 type Cost = Int
@@ -111,7 +120,7 @@ readProfile file = do
   let parse hdl stime prof = do
         stop <- hIsEOF hdl
         if not stop then do
-            (stime',prof') <- accumProfile stime prof <$> hGetLine hdl
+            (stime',prof') <- accumProfile stime prof <$> S.hGetLine hdl
             parse hdl stime' prof'
           else return prof
 
@@ -124,27 +133,34 @@ program to analyse and create an accumulator in the background that we
 can look at whenever we want using the reading action returned by the
 starter function 'profile'. The 'ProcessHandle' is also returned in
 order to allow the caller to control the execution of the slave
-process. Note that you can take a command line given in a string and
-convert it into a 'CreateProcess' bundle using
+process as well as an IO action that stops the reading thread without
+touching the slave process. Note that you can take a command line
+given in a string and convert it into a 'CreateProcess' bundle using
 'System.Process.shell'. -}
 
-profile :: CreateProcess -> IO (ProfileReader,ProcessHandle)
+profile :: CreateProcess -> IO (ProfileReader, ProcessHandle, IO ())
 profile prog = do
   let getCmd p = case cmdspec p of
                    ShellCommand cmd -> cmd
                    RawCommand prg args -> intercalate " " (prg:args)
   zt <- getZonedTime
-  ref <- newIORef emptyProfile {
-                 profJob = getCmd prog,
-                 profDate = formatTime defaultTimeLocale "%F %H:%M:%S %Z" zt
+  ref <- newIORef emptyProfile
+               { profJob = getCmd prog
+               -- The time format is deliberately different from the
+               -- one currently used in heap profiles. Changing it
+               -- doesn't hurt anyone, and it makes more sense this
+               -- way, so there.
+               , profDate = formatTime defaultTimeLocale "%F %H:%M:%S %Z" zt
                }
-  hdl <- profileCallback prog $ \pkg -> do
+  (hdl,act) <- profileCallback prog $ \pkg -> do
     prof <- readIORef ref
     case pkg of
-      SinkSample t smp   -> writeIORef ref $ prof { profData = M.insert t smp (profData prof) }
-      SinkId ccid ccname -> writeIORef ref $ prof { profNames = IM.insert ccid ccname (profNames prof) }
+      SinkSample t smp   -> writeIORef ref $ prof
+                            { profData = M.insert t smp (profData prof) }
+      SinkId ccid ccname -> writeIORef ref $ prof
+                            { profNames = IM.insert ccid ccname (profNames prof) }
       _                  -> return ()
-  return (readIORef ref,hdl)
+  return (readIORef ref,hdl,act)
 
 {-| We might not want to hold on to all the past output, just do some
 stream processing. We can achieve this using a callback function
@@ -162,49 +178,68 @@ data SinkInput = SinkSample Time ProfileSample
 
 {-| The 'profileCallback' function initiates an observation without
 maintaining an internal data, passing profile samples to the callback
-as they come. -}
+as they come. It returns the handle of the new process as well as the
+thread stopper action. -}
 
-profileCallback :: CreateProcess -> ProfileSink -> IO ProcessHandle
+profileCallback :: CreateProcess -> ProfileSink -> IO (ProcessHandle, IO ())
 profileCallback prog sink = do
+  dir <- getCurrentDirectory
+  let hpPath = fromMaybe dir (cwd prog) ++
+               '/' : (takeFileName . execPath . cmdspec) prog ++ ".hp"
+      -- Yes, this is extremely naive, but it will do for the time being...
+      execPath (ShellCommand cmd) = takeWhile (/=' ') cmd
+      execPath (RawCommand path _) = path
+
+  -- We have to delete the .hp file and wait for the process to create it.
+  catch (removeFile hpPath) (const (return ()))
   (_,_,_,phdl) <- createProcess prog
+
+  stopRequest <- newIORef False
+
   -- Question: do we want an alternative single-threaded interface?
   forkIO $ do
-    let -- We might actually need getCurrentDirectory instead of using ".".
-        hpPath = maybe "." id (cwd prog) ++ '/' : (takeFileName . execPath . cmdspec) prog ++ ".hp"
-        -- Yes, this is extremely naive, but it will do for the time being...
-        execPath (ShellCommand cmd) = takeWhile (/=' ') cmd
-        execPath (RawCommand path _) = path        
     maybeHpFile <- tryRepeatedly (openFile hpPath ReadMode) 50 10000
     let hpFile = fromJust maybeHpFile
-        pass line idmap smp = do
-          stop <- getProcessExitCode phdl
-          if stop == Nothing then do
-              eof <- hIsEOF hpFile
-              if eof then do
-                  threadDelay 1000
-                  pass line idmap smp
+        pass buf idmap smp = do
+          let mlen = S.elemIndex '\n' buf
+          stop <- readIORef stopRequest
+          if mlen /= Nothing && not stop then do
+              -- If there's a whole line in the buffer and we still
+              -- have the green light, we'll parse it and notify the
+              -- callback (sink) if necessary.
+              let len = fromJust mlen
+                  rest = S.drop (len+1) buf
+              case parseHpLine (S.take len buf) of
+                BeginSample _ -> pass rest idmap []
+                EndSample t -> do
+                  when (not (null smp)) $ sink (SinkSample t smp)
+                  pass rest idmap []
+                Cost ccname cost -> do
+                  let (newid,ccid,idmap') = addCCId idmap ccname
+                  when newid $ sink (SinkId ccid ccname)
+                  pass rest idmap' ((ccid,cost):smp)
+                _ -> pass rest idmap smp
+            else do
+              slaveCode <- getProcessExitCode phdl
+              if slaveCode == Nothing && not stop then do
+                  -- If there's no line known to be full while the
+                  -- other process is still running, we keep trying to
+                  -- fetch more data.
+                  eof <- hIsEOF hpFile
+                  if eof then do
+                      threadDelay 100000
+                      pass buf idmap smp
+                    else do
+                      newChars <- S.hGetNonBlocking hpFile 0x10000
+                      pass (S.append buf newChars) idmap smp
                 else do
-                  -- Setting line buffering doesn't help me make this work...
-                  --res <- parseHpLine <$> hGetLine hpFile
-                  c <- hGetChar hpFile
-                  if c == '\n' then do
-                      case parseHpLine (reverse line) {- res -} of
-                        BeginSample _      -> pass "" idmap []
-                        EndSample t        -> do when (not (null smp)) $ sink (SinkSample t smp)
-                                                 pass "" idmap []
-                        Sample ccname cost -> do let (newid,ccid,idmap') = addCCId idmap ccname
-                                                 when newid $ sink (SinkId ccid ccname)
-                                                 pass "" idmap' ((ccid,cost):smp)
-                        _ -> pass "" idmap smp
-                    else pass (c:line) idmap smp
-            else do sink SinkStop
-                    return ()
+                  -- At this point either the other process ended or
+                  -- we were requested to stop reading further.
+                  sink SinkStop
+                  return ()
     -- Again, we should use a trie here instead of a Map String...
-    when (maybeHpFile /= Nothing) $ do                              
-      -- Line buffering doesn't seem to help with input streams...
-      --hSetBuffering hpFile LineBuffering
-      pass "" M.empty []
-  return phdl
+    when (maybeHpFile /= Nothing) $ pass S.empty T.empty []
+  return (phdl,writeIORef stopRequest True)
 
 tryRepeatedly :: IO a -> Int -> Int -> IO (Maybe a)
 tryRepeatedly act n d | n < 1     = return Nothing
@@ -252,45 +287,47 @@ data ParseResult = Unknown
                  | Date String
                  | BeginSample Time
                  | EndSample Time
-                 | Sample CostCentreName Cost
+                 | Cost CostCentreName Cost
 
-parseHpLine :: String -> ParseResult
-parseHpLine line = if null cost then head ([val | (key,val) <- results, key == cmd] ++ [Unknown])
-                   else Sample ccname (read (tail cost))
-    where (ccname,cost) = span (/='\t') line
-          (cmd,_:param) = span (/=' ') line
-          results = [("JOB",Job (read param)),
-                     ("DATE",Date (read param)),
-                     ("BEGIN_SAMPLE",BeginSample (read param)),
-                     ("END_SAMPLE",EndSample (read param))]
+parseHpLine :: ByteString -> ParseResult
+parseHpLine line = if S.null cost then head ([val | (key,val) <- results, key == cmd] ++ [Unknown])
+                   else Cost ccname (read . S.unpack . S.tail $ cost)
+    where (ccname,cost) = S.span (/='\t') line
+          (cmd,sparam) = S.span (/=' ') line
+          param = S.unpack (S.tail sparam)
+          results = if S.null sparam then [] else
+                        [(S.pack "JOB",Job (read param)),
+                         (S.pack "DATE",Date (read param)),
+                         (S.pack "BEGIN_SAMPLE",BeginSample (read param)),
+                         (S.pack "END_SAMPLE",EndSample (read param))]
 
-accumProfile :: Maybe Time -> Profile -> String -> (Maybe Time,Profile)
+accumProfile :: Maybe Time -> Profile -> ByteString -> (Maybe Time,Profile)
 accumProfile time prof line = case parseHpLine line of
-  Job s              -> (Nothing,prof { profJob = s })
-  Date s             -> (Nothing,prof { profDate = s })
-  BeginSample t      -> (Just t,prof)
-  EndSample _        -> (Nothing,prof)
-  Sample ccname cost -> let (newid,ccid,pnsi') = addCCId (profNamesInv prof) ccname
-                        in (time,
-                            prof {
-                              profData = M.insertWith (++) (fromJust time) [(ccid,cost)] (profData prof),
-                              profNames = if newid then IM.insert ccid ccname (profNames prof) else profNames prof,
-                              profNamesInv = pnsi'
-                            })
-  Unknown            -> (Nothing,prof)
+  Job s            -> (Nothing,prof { profJob = s })
+  Date s           -> (Nothing,prof { profDate = s })
+  BeginSample t    -> (Just t,prof)
+  EndSample _      -> (Nothing,prof)
+  Cost ccname cost -> let (newid,ccid,pnsi') = addCCId (profNamesInv prof) ccname
+                      in (time,
+                          prof
+                          { profData = M.insertWith (++) (fromJust time) [(ccid,cost)] (profData prof)
+                          , profNames = if newid then IM.insert ccid ccname (profNames prof) else profNames prof
+                          , profNamesInv = pnsi'
+                          })
+  Unknown          -> (Nothing,prof)
 
-addCCId :: ProfNamesInv -> CostCentreName -> (Bool, CostCentreId, ProfNamesInv)
-addCCId idmap ccname = if ccid /= M.size idmap then (False,ccid,idmap)
-                       else (True,ccid,M.insert ccname ccid idmap)
-    where ccid = M.findWithDefault (M.size idmap) ccname idmap
+addCCId :: Trie CostCentreId -> CostCentreName -> (Bool, CostCentreId, Trie CostCentreId)
+addCCId idmap ccname = if ccid /= T.size idmap then (False,ccid,idmap)
+                       else (True,ccid,T.insert ccname ccid idmap)
+    where ccid = fromMaybe (T.size idmap) (T.lookup ccname idmap)
 
 -- Some tests --
 
 -- For the time being, we assume that getCurrentDirectory returns the
 -- dir of the cabal file, because we love emacs.
 
--- Callback test
-test1 :: IO ProcessHandle
+-- Callback test (also: stopTest <- snd <$> test1)
+test1 :: IO (ProcessHandle, IO ())
 test1 = do
   dir <- getCurrentDirectory
   profileCallback (shell (dir++"/test/tester")) { cwd = Just (dir++"/test") } print
@@ -299,7 +336,7 @@ test1 = do
 test2 :: IO ()
 test2 = do
   dir <- getCurrentDirectory
-  (reader,_) <- profile (shell (dir++"/test/tester")) { cwd = Just (dir++"/test") }
+  (reader,_,_) <- profile (shell (dir++"/test/tester")) { cwd = Just (dir++"/test") }
   replicateM_ 4 $ do
     prof <- reader
     print prof
